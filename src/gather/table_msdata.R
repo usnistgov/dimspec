75b8bd144b2e8e4d8af1b141c4260091356eec21
Place <bjp@NIST.GOV>
2021-06-29 16:22:12 -0400

    added peak gathering function

--- src/gather/table_msdata.R
+++ src/gather/table_msdata.R
@@ -1,3 +1,4 @@
 table_msdata <- function(mzml, scans) {
+  if (length(scans) == 0) return(NULL)
   do.call(rbind, lapply(scans, function(x) data.frame(scan = x, scantime = gettime(mzml, x), msdata = zipms(extract.ms(mzml, x))  )))
 }
---
+++ src/methodreportingtool/parse_methodxml.R
@@ -0,0 +1,5 @@
+library(XML)
+
+parse_methodxml <- function(xml) {
+  xmlToList(xml)
+}
