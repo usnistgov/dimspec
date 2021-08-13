b8ff1b3ab21406be7ebe3519aea093c84af8fdc6
Jared R <jared.ragland@nist.gov>
2021-08-13 14:26:56 -0400

    Connection management refinements and function documentation. Add class check to verify_args.

--- src/app_functions.R
+++ src/app_functions.R
@@ -97,12 +97,14 @@ mode_checks <- function(prefix = "is", use_deprecated = FALSE) {
 #' useful for development of shiny applications. Values from other sources MUST
 #' be coercible to a full list (e.g. if being parsed from JSON, use
 #' \code{jsonlite::fromJSON(simplifyMatrix = FALSE))}
-#' 
+#'
 #' If logger is enabled, also provides some additional meaningful feedback.
+#'
+#' At least one condition check is required for every element passed to `args`.
 #'
 #' @param args LIST of named arguments and their values, typically passed
 #'   directly from a function definition in the form \code{args = list(foo =
-#'   1:2, bar = c("a", "b", "c"))}
+#'   1:2, bar = c("a", "b", "c"))} or directly by passing \code{environment()}
 #' @param conditions Nested LIST of conditions and values to check, with one
 #'   list item for each element in \code{args}. \itemize{\item The first element
 #'   of each list should be a character scalar in the supported list. \item The
@@ -111,8 +113,13 @@ mode_checks <- function(prefix = "is", use_deprecated = FALSE) {
 #'   element of \code{args} in the form \itemize{\item \code{conditions =
 #'   list(foo = list(c("mode", "numeric"), c("length", 2)), bar = list(c("mode",
 #'   "character"), c("n<", 5)))}} Currently supported expectations are:\itemize{
-#'   \item \preformatted{"mode"    checks class expectation by applying the
-#'   \code{is.X} or the \code{is_X} family of functions either directly or
+#'   \item \preformatted{"class"    checks strict class expectation by direct
+#'   comparison with \code{class} to support object classes not supported with
+#'   the \code{is.x} or \code{is_x} family of functions; much stricter than a
+#'   "mode" check in that the requested check must be present in the return from
+#'   a call to \code{class} e.g. "list" will fail if a "data.frame" object is
+#'   passed \item \preformatted{"mode"    checks class expectation by applying
+#'   the \code{is.X} or the \code{is_X} family of functions either directly or
 #'   flexibly depending on the value provided to \code{conditions} (e.g.
 #'   c("mode", "character") and c("mode", "is.character") and c("mode",
 #'   "is_character") all work equally well) and will default to the version you
@@ -120,29 +127,29 @@ mode_checks <- function(prefix = "is", use_deprecated = FALSE) {
 #'   "is.character" simply provide "is_character" as the condition. Only those
 #'   modes able to be checked by this family of functions are supported. Run
 #'   function \code{mode_checks()} for a complete sorted list for your current
-#'   configuration.} \item \preformatted{"length"  length of values matches a
+#'   configuration.} \item \preformatted{"length" length of values matches a
 #'   pre-determined exact length, typically a single value expectation (e.g.
-#'   c("length",#'   1))} \item \preformatted{"no_na"   no NA values are
-#'   present} \item \preformatted{"n>"      length of values is greater than a
-#'   given value - "n<" length of values is lesser than a given value} \item
+#'   c("length",#'   1))} \item \preformatted{"no_na" no NA values are present}
+#'   \item \preformatted{"n>"      length of values is greater than a given
+#'   value - "n<" length of values is lesser than a given value} \item
 #'   \preformatted{"n>="     length of values is greater than or equal to a
 #'   given value} \item \preformatted{"n<="     length of values is lesser than
-#'   or equal to a given value} \item \preformatted{">"       numeric or date
-#'   value is greater than a given value} \item \preformatted{"<"       numeric
-#'   or date value is greater than a given value} \item \preformatted{">="
-#'   numeric or date value is greater than or equal to a given value} \item
-#'   \preformatted{"<="      numeric or date value is lesser than or equal to a
-#'   given value} \item \preformatted{"between" numeric or date values are bound
-#'   within an INCLUSIVE range (e.g. c("range", 1:5))} \item
-#'   \preformatted{"choices" provided values are part of a selected list of
-#'   expectations (e.g. \code{c("choices", list(letters[1:3]))})} \item
-#'   \preformatted{"FUN"     apply a function to the value and check that the
-#'   result is valid or that the function can be executed without error; this
-#'   evaluates the check condition using \code{tryCatch} via \code{do.call} and
-#'   so can also accept a full named list of arg values. This is a strict check
-#'   in the sense that a warning will also result in a failed result, passing
-#'   the warning (or error if the function fails) message back to the user, but
-#'   does not halt checks} }
+#'   or equal to a given value} \item \preformatted{">" numeric or date value is
+#'   greater than a given value} \item \preformatted{"<" numeric or date value
+#'   is greater than a given value} \item \preformatted{">=" numeric or date
+#'   value is greater than or equal to a given value} \item \preformatted{"<="
+#'   numeric or date value is lesser than or equal to a given value} \item
+#'   \preformatted{"between" numeric or date values are bound within an
+#'   INCLUSIVE range (e.g. c("range", 1:5))} \item \preformatted{"choices"
+#'   provided values are part of a selected list of expectations (e.g.
+#'   \code{c("choices", list(letters[1:3]))})} \item \preformatted{"FUN" apply a
+#'   function to the value and check that the result is valid or that the
+#'   function can be executed without error; this evaluates the check condition
+#'   using \code{tryCatch} via \code{do.call} and so can also accept a full
+#'   named list of arg values. This is a strict check in the sense that a
+#'   warning will also result in a failed result, passing the warning (or error
+#'   if the function fails) message back to the user, but does not halt checks}
+#'   }
 #' @param from_fn CHR scalar of the function from which this is called, used if
 #'   logger is enabled and ignored if not. (default NULL)
 #'
@@ -180,7 +187,7 @@ verify_args <- function(args, conditions, from_fn = NULL) {
   if (logger) log_trace('Verifying arguments{from_fn}.')
   require(glue)
   if (length(args) != length(conditions)) stop('Each item in "args" needs at least one matching condition.')
-  check_types  <- c("mode", "length", "no_na", "n>", "n<", "n>=", "n<=", ">", "<", ">=", "<=", "between", "choices", "FUN")
+  check_types  <- c("class", "mode", "length", "no_na", "n>", "n<", "n>=", "n<=", ">", "<", ">=", "<=", "between", "choices", "FUN")
   supported    <- paste0("'", check_types, "'", collapse = ", ")
   mode_types   <- mode_checks()
   out          <- list()
@@ -201,6 +208,10 @@ verify_args <- function(args, conditions, from_fn = NULL) {
       type  <- unlist(x[1])
       check <- unlist(x[2])
       switch(type,
+             "class"   = {
+               rslt <- check %in% class(val)
+               msg  <- glue("'{names(arg)}' must be of class '{check} rather than '{class(val)}'.")
+             },
              "mode"    = {
                mode_check <- grep(paste0("is[\\._]", check), mode_types, value = TRUE)
                if (length(mode_check) > 0) {
@@ -338,10 +349,24 @@ verify_args <- function(args, conditions, from_fn = NULL) {
   return(out)
 }
 
-#' WIP Rebuild the database from scratch
+#' Build or rebuild the database from scratch
+#'
+#' This function will build or rebuild the NIST HRAMS database structure from
+#' scratch, removing the existing instance. By default, most parameters are set
+#' in the environment (at "./config/env_glob.txt") but any values can be passed
+#' directly. This can be used to quickly spin up multiple copies with a clean
+#' slate using different build files, data files, or return to the last stable
+#' release.
+#'
+#' If sqlite3 and its command line interface are available on your platform,
+#' that will be used (preferred method) but, if not, this function will read in
+#' all the necessary files to directly create it using shell commands. The shell
+#' method may not be universally applicable to certain compute environments or
+#' may require elevated permissions.
 #'
 #' @param db CHR scalar of the database name
-#' @param build_from CHR scalar of a SQL build script to use (default "build.sql")
+#' @param build_from CHR scalar of a SQL build script to use (default
+#'   "build.sql")
 #' @param populate_with CHR scalar for which data to include in the build; valid
 #'   choices are "demo" and "pfas" (default "demo")
 #' @param archive BOOL scalar of whether to create an archive of the current
@@ -349,17 +374,20 @@ verify_args <- function(args, conditions, from_fn = NULL) {
 #'   (default FALSE), passed to `remove_db()`
 #' @param sqlite_cli CHR scalar to use to look for installed sqlite3 CLI tools
 #'   in the current system environment (default "sqlite3")
+#' @param connect BOOL scalar of whether or not to connect to the rebuilt
+#'   database in the global environment as object `con`` (default FALSE)
 #'
 #' @return None, check console for details
 #' @export
 #'
 #' @examples
-rebuild_db <- function(db            = DB_NAME,
-                       build_from    = DB_BUILD_FILE,
-                       populate      = TRUE,
-                       populate_with = DB_DATA,
-                       archive       = FALSE,
-                       sqlite_cli    = SQLITE_CLI) {
+build_db <- function(db            = DB_NAME,
+                     build_from    = DB_BUILD_FILE,
+                     populate      = TRUE,
+                     populate_with = DB_DATA,
+                     archive       = FALSE,
+                     sqlite_cli    = SQLITE_CLI,
+                     connect       = FALSE) {
   # Argument validation
   arg_check <- verify_args(
     args       = as.list(environment()),
@@ -369,9 +397,10 @@ rebuild_db <- function(db            = DB_NAME,
       populate      = list(c("mode", "logical"), c("length", 1)),
       populate_with = list(c("mode", "character"), c("length", 1)),
       archive       = list(c("mode", "logical"), c("length", 1)),
-      sqlite_cli    = list(c("mode", "character"), c("length", 1))
+      sqlite_cli    = list(c("mode", "character"), c("length", 1)),
+      connect       = list(c("mode", "logical"), c("length", 1))
     ),
-    from_fn    = "rebuild_db"
+    from_fn    = "build_db"
   )
   if (!arg_check$valid) {
     stop(cat(paste0(arg_check$messages, collapse = "\n")))
@@ -390,16 +419,20 @@ rebuild_db <- function(db            = DB_NAME,
       stop(glue('Cannot locate file "{build_file}" in this directory.'))
     }
     build         <- glue('-cmd ".read {build_file}"')
-    populate_file <- list.files(pattern = populate_with, full.names = TRUE, recursive = TRUE)
-    if (!file.exists(populate_file)) {
-      log_warn('Cannot locate file "{populate_file}" in this directory; "{db}" will be created but not populated.')
-      populate    <- ""
+    if (populate) {
+      populate_file <- list.files(pattern = populate_with, full.names = TRUE, recursive = TRUE)
+      if (!file.exists(populate_file)) {
+        log_warn('Cannot locate file "{populate_file}" in this directory; "{db}" will be created but not populated.')
+        populate_cmd <- ""
+      } else {
+        populate_cmd <- glue(' -cmd ".read {populate_file}"')
+      }
     } else {
-      populate    <- glue(' -cmd ".read {populate_file}"')
+      populate_cmd <- ""
     }
-    full_call     <- glue('{build}{populate}') %>%
+    full_call     <- glue('{build}{populate_cmd}') %>%
       str_remove_all("\\./")
-    full_call     <- glue('{sqlite_call} {full_call} -cmd .exit')
+    full_call     <- glue('{sqlite_call} {full_call} -cmd ".exit"')
     log_trace("Issuing shell command {full_call}")
     if (file.exists(db)) remove_db(db = db, archive = archive)
     shell(full_call)
@@ -441,6 +474,9 @@ rebuild_db <- function(db            = DB_NAME,
     # Clean up
     dbDisconnect(con)
   }
+  if (connect) {
+    con <<- dbConnect(RSQLite::SQLite(), db)
+  }
 }
   
 #' Remove an existing database
@@ -477,23 +513,24 @@ remove_db <- function(db = DB_NAME, archive = FALSE) {
   }
   # Resolve database file location
   if (logger) log_trace('Finding database file "{db}"')
-  where  <- list.files(pattern = db, full.names = TRUE, recursive = TRUE)
-  if (length(where) > 0) {
+  db_path  <- list.files(pattern = db, full.names = TRUE, recursive = TRUE)
+  db_path  <- db_path[basename(db_path) == db]
+  if (length(db_path) > 0) {
     # Ensure no current connection from R
-    check <- tryCatch(manage_connection(db = db, disconnect = TRUE))
-    if (class(check) == "try-error") stop("Unable to automatically stop the connection to", db, ".")
+    check <- try(manage_connection(db = db, reconnect = FALSE, disconnect = TRUE))
+    if (class(check) == "try-error") stop("Unable to automatically stop the connection to '", db, "'.")
     if (archive) {
       now       <- format(Sys.time(), "%Y%m%d%H%M%S%Z")
       fname     <- file_path_sans_ext(db)
       new_fname <- gsub(fname,
                         sprintf("%s_archive_%s",fname, now),
                         db)
-      file.copy(where, new_fname)
+      file.copy(db_path, new_fname)
       if (logger) log_success('Archive created as "{new_fname}"')
     }
-    result <- tryCatch(file.remove(where))
+    result <- try(file.remove(db_path))
     if (class(result) == "try-error") {
-      if (logger) log_error('Database "{db}" could not be removed; another connection is likely open elsewhere.')
+      if (logger) log_error('Database "{db}" could not be removed; another connection is likely open elsedb_path.')
     } else {
       if (logger) log_success('Database "{db}" removed.')
     }
@@ -509,6 +546,9 @@ remove_db <- function(db = DB_NAME, archive = FALSE) {
 #' @param drv CHR vector of connection object classes to check against. Note
 #'   this may depend heavily on connection packages and must be present in the
 #'   class names of the driver used. (default "SQLiteConnection")
+#' @param reconnect LGL scalar of whether or not to connect if a connection does
+#'   not exist (default TRUE); if both this and `disconnect` are true, it will
+#'   first be disconnected
 #' @param disconnect LGL scalar of whether or not to terminate and remove the
 #'   connection from the current global environment (default TRUE)
 #'
@@ -516,13 +556,18 @@ remove_db <- function(db = DB_NAME, archive = FALSE) {
 #' @export
 #'
 #' @examples
-manage_connection <- function(db = DB_NAME, drv = "SQLiteConnection", disconnect = TRUE) {
+manage_connection <- function(db = DB_NAME,
+                              drv_pack = "RSQLite",
+                              drv = "SQLite",
+                              check_class = "SQLiteConnection",
+                              reconnect = TRUE,
+                              disconnect = TRUE) {
   logger <- "logger" %in% (.packages())
   current_env <- as.list(.GlobalEnv)
   current_env_names <- names(current_env)
   for (env in current_env_names) {
     this_obj <- current_env[[env]]
-    if (any(drv %in% class(this_obj))) {
+    if (any(check_class %in% class(this_obj))) {
       if (basename(this_obj@dbname) == db) {
         if (logger) log_trace('Database "{db}" is currently open.')
         if (disconnect) {
@@ -531,6 +576,19 @@ manage_connection <- function(db = DB_NAME, drv = "SQLiteConnection", disconnect
           rm(list = env, pos = ".GlobalEnv")
         }
       }
+    }
+  }
+  if (reconnect) {
+    # Resolve database file location
+    if (logger) log_trace('Finding database file "{db}"')
+    where  <- list.files(pattern = db, full.names = TRUE, recursive = TRUE)
+    if (length(where) == 0) {
+      stop(sprintf("Unable to locate '%s'.", db))
+    }
+    require(drv_pack, character.only = TRUE)
+    con <<- try(dbConnect(do.call(drv, list()), db))
+    if (class(con) == "try-error") {
+      stop(sprintf("Unable to connect to '%s'.", where))
     }
   }
 }
@@ -568,4 +626,30 @@ build_db_logging_triggers <- function(db = DB_NAME, connection = "con", log_tabl
       )
     }
   }
+}
+
+#' Grammatically collapse a list of values
+#'
+#' Given a vector of arbitrary length that coerces properly to a human-readable
+#' character string, return it formatted as one of: "one", "one and two", or
+#' "one, two, ..., and three" using `glue::glue`
+#'
+#' @param namelist vector of values to format
+#' 
+#' @return CHR vector of length one
+#' @export
+#' 
+#' @examples
+#' format_list_of_names("test")
+#' format_list_of_names(c("apples", "bananas"))
+#' format_list_of_names(c(1:3))
+#' format_list_of_names(seq.Date(Sys.Date(), Sys.Date() + 3, by = 1))
+format_list_of_names <- function(namelist) {
+  if ( length(namelist) == 1 )
+    return(glue::glue("{paste0(namelist, collapse = '')}"))
+  res <- glue::glue(
+    "{paste0(namelist[1:length(namelist) - 1], collapse = ', ')}\\
+     {ifelse(length(namelist) > 2, ',', '')} \\
+     {ifelse(length(namelist) > 1, paste('and ', namelist[length(namelist)], sep = ''), '')}")
+  return(res)
 }
