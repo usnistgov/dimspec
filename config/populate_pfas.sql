8915531824b04de989bd85dad9ca53963169adfe
Jared R <jared.ragland@nist.gov>
2021-09-01 09:15:30 -0400

    Reorder pfas population script to comply with FK enforcement.

--- config/populate_pfas.sql
+++ config/populate_pfas.sql
@@ -2,7 +2,7 @@
 	Run this file in sqlite3 to load basic data regarding PFAS into the 
 	database schema using:
 	
-		.read config/setup_pfas.sql
+		.read config/populate_pfas.sql
 	
 	Note this will first remove all data within the referenced tables!
 	You may also directly import these files into the appropriate tables using 
@@ -14,34 +14,39 @@
 
 /* To build in console, use each of the following as needed. */
 
-delete from norm_source_types;
-.import --csv --skip 1 config/data/norm_source_types.csv norm_source_types
-delete from compounds;
-.import --csv --skip 1 config/data/pfas/compounds.csv compounds
-delete from compound_aliases;
-.import --csv --skip 1 config/data/pfas/compound_aliases.csv compound_aliases
-delete from compound_alias_references;
-.import --csv --skip 1 config/data/pfas/compound_alias_references.csv compound_alias_references
-delete from mobile_phases;
-.import --csv --skip 1 config/data/pfas/mobile_phases.csv mobile_phases
-delete from ms_descriptions;
-.import --csv --skip 1 config/data/pfas/ms_descriptions.csv ms_descriptions
-delete from ms_methods;
-.import --csv --skip 1 config/data/pfas/ms_methods.csv ms_methods
-delete from norm_ms_types;
-.import --csv --skip 1 config/data/pfas/norm_ms_types.csv norm_ms_types
-delete from norm_solvents;
-.import --csv --skip 1 config/data/pfas/norm_solvents.csv norm_solvents 
-delete from norm_sample_classes;
+DELETE FROM solvent_mixes;
+DELETE FROM solvent_mix_collections;
+DELETE FROM solvent_aliases;
+DELETE FROM mobile_phases;
+DELETE FROM norm_solvents;
+DELETE FROM samples;
+DELETE FROM norm_sample_classes;
+DELETE FROM norm_ms_types;
+DELETE FROM ms_methods;
+DELETE FROM ms_descriptions;
+.read config/populate_common.sql
+.read config/sql_nodes/contributors_data.sql
+DELETE FROM compounds;
+DELETE FROM compound_categories;
+DELETE FROM compound_alias_references;
+DELETE FROM compound_aliases;
+DELETE FROM conversion_software_linkage;
+DELETE FROM conversion_software_settings;
+.import --csv --skip 1 config/data/pfas/norm_solvents.csv norm_solvents
 .import --csv --skip 1 config/data/pfas/norm_sample_classes.csv norm_sample_classes
-delete from affiliations;
+.import --csv --skip 1 config/data/pfas/norm_ms_types.csv norm_ms_types
+.import --csv --skip 1 config/data/pfas/ms_methods.csv ms_methods
+.import --csv --skip 1 config/data/pfas/ms_descriptions.csv ms_descriptions
 .import --csv --skip 1 config/data/pfas/affiliations.csv affiliations
-delete from contributors;
 .import --csv --skip 1 config/data/pfas/contributors.csv contributors
-delete from samples;
-.import --csv --skip 1 config/data/pfas/samples.csv samples
-delete from solvent_aliases;
+.import --csv --skip 1 config/data/pfas/solvent_mix_collections.csv solvent_mix_collections
+.import --csv --skip 1 config/data/pfas/mobile_phases.csv mobile_phases
 .import --csv --skip 1 config/data/pfas/solvent_aliases.csv solvent_aliases
-delete from solvent_mixes;
 .import --csv --skip 1 config/data/pfas/solvent_mixes.csv solvent_mixes
-.read config/data/pfas/methods_data.sql
+.import --csv --skip 1 config/data/pfas/compound_categories.csv compound_categories
+.import --csv --skip 1 config/data/pfas/compounds.csv compounds
+.import --csv --skip 1 config/data/pfas/compound_alias_references.csv compound_alias_references
+.import --csv --skip 1 config/data/pfas/compound_aliases.csv compound_aliases
+.import --csv --skip 1 config/data/pfas/conversion_software_linkage.csv conversion_software_linkage
+.import --csv --skip 1 config/data/pfas/conversion_software_settings.csv conversion_software_settings
+.import --csv --skip 1 config/data/pfas/samples.csv samples
