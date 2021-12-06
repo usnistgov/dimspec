2e8f3ff7437f3bbad4a9b4c03ffbeaedc81fa238
Jared R <jared.ragland@nist.gov>
2021-12-06 14:02:32 -0500

    Revert "Roll back renv merge and save it different branch."
    This reverts commit 91567a68e88655a358241f073d35bb9394ac0386.

--- config/env_R.R
+++ config/env_R.R
@@ -2,7 +2,7 @@
 
 # Current version of the database and the date it was created (if present)
 DB_DATE        <- file.info(list.files(pattern = sprintf("%s$", DB_NAME),
-                                       recursive = FALSE))$ctime
+                                       recursive = TRUE))$ctime
 DB_BUILT       <- length(DB_DATE) > 0
 # Releases will be coded by:
 #   - First position: major version (e.g. schema changes)
@@ -23,32 +23,12 @@ DB_PACKAGE     <- "RSQLite"
 DB_DRIVER      <- "SQLite"
 DB_CLASS       <- "SQLite"
 DICT_FILE_NAME <- "data_dictionary"
-INCLUDE_DICT   <- TRUE
-INCLUDE_MAP    <- TRUE
-
-# Set this to TRUE to isolate environment to the renv.lock file
-USE_RENV_LOCK  <- TRUE
 
 # The last time the main database schema defined in BUILD_FILE was updated
-LAST_DB_SCHEMA <- file.info(
-  list.files(path = "config",
-             pattern = DB_BUILD_FILE,
-             recursive = FALSE)
-)$mtime
+LAST_DB_SCHEMA <- file.info(list.files(pattern = DB_BUILD_FILE, recursive = TRUE))$mtime
 
-# The last time any file in this project's config directory was modified
-search_paths <- list.dirs(recursive = FALSE)
-path_exclude <- "^\\./\\.[[:alnum:]]*|renv|data/backups"
-search_paths <- search_paths[-grep(path_exclude, search_paths)]
-LAST_MODIFIED  <- max(
-  file.info(
-    list.files(path = search_paths,
-               recursive = TRUE,
-               full.names = TRUE
-    )
-  )$mtime,
-  na.rm = TRUE)
-rm(search_paths, path_exclude)
+# The last time any file in this project was modified
+LAST_MODIFIED  <- max(file.info(list.files(recursive = TRUE))$mtime)
 
 # Set logger threshold.
 LOG_THRESHOLD  <- "INFO"
@@ -59,21 +39,21 @@ DEPENDS_ON     <- c("base64enc",
                     "logger",
                     "DBI",
                     "RSQLite",
+                    "lubridate",
                     "glue",
                     "stringi",
-                    "ggplot2",
-                    "tibble",
-                    "tidyr",
-                    "readr",
-                    "purrr",
-                    "dplyr",
-                    "stringr",
-                    "forcats",
+                    "tidyverse",
+                    "dbplyr",
                     "jsonlite",
-                    "rlang",
                     "tools",
                     "xlsx",
                     "XML")
+
+# Also needs ChemmineR, which is only available through Bioconductor
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager")
+  BiocManager::install("ChemmineR")
+}
 
 # Files matching these patterns will be excluded from sourcing.
 EXCLUSIONS     <- c(".RDS",
