f1763c624df27117cbbca6016f0d48ab750dad39
Jared R <jared.ragland@nist.gov>
2022-01-21 13:48:55 -0500

    Add documentation and validation checks to function sin api_control and add object passing as an option to obj_name_check.

--- plumber/api_control.R
+++ plumber/api_control.R
@@ -1,13 +1,64 @@
 #' Start the plumber API
 #'
+#' This is a thin wrapper to [plumber::pr_run] pointing to a project's plumber
+#' file by default at "plumber/plumber.R" with some error trapping. It also
+#' provides some infrastructure support for set ups with a "plumber/env_plumb.R"
+#' file containing environment variables that will be used as defaults
+#' supporting NULL assignments.
+#'
 #' @param on_host CHR scalar of the host IP address
 #' @param on_port CHR or INT scalar of the host port to use
+#' @param plumber_file CHR scalar of the path to a plumber API to launch
+#'   (default: `file.path("plumber", "plumber.R")`)
 #'
 #' @return None, launches the plumber API server
 #' @export
-api_start <- function(on_host, on_port) {
+api_start <- function(on_host = NULL, on_port = NULL, plumber_file = file.path("plumber", "plumber.R")) {
+  if (is.null(on_host)) {
+    if (exists("PLUMBER_HOST")) {
+      on_host <- PLUMBER_HOST
+    } else {
+      if (exists("log_it")) {
+        log_it("error", 'Provide a host IP address for "on_host" or set variable "PLUMBER_HOST" in the "plumber/env_plumb.R" file.')
+        return(invisible(NULL))
+      }
+    }
+  }
+  if (is.null(on_port)) {
+    if (exists("PLUMBER_PORT")) {
+      on_port <- PLUMBER_PORT
+    } else {
+      if (exists("log_it")) {
+        log_it("error", 'Provide a port number for "on_port" or set variable "PLUMBER_PORT" in the "plumber/env_plumb.R" file.')
+        return(invisible(NULL))
+      }
+    }
+  }
+  
+  # Argument validation relies on verify_args
+  if (exists("verify_args")) {
+    arg_check <- verify_args(
+      args       = as.list(environment()),
+      conditions = list(
+        on_host      = list(c("mode", "character"), c("length", 1)),
+        on_port      = list(c("length", 1)),
+        plumber_file = list(c("mode", "character"), "file_exists", c("length", 1))
+      ),
+      from_fn = "api_start"
+    )
+    stopifnot(arg_check$valid)
+  } else {
+    stopifnot(is.character(on_host))
+    stopifnot(length(on_host) == 1)
+    stopifnot(is.character(on_port))
+    stopifnot(length(on_port) == 1)
+    stopifnot(is.character(plumber_file))
+    stopifnot(length(plumber_file) == 1)
+    stopifnot(file.exists(plumber_file))
+  }
+  
   plumber::pr_run(
-    pr = plumber::pr(file.path("plumber", "plumber.R")),
+    pr = plumber::pr(plumber_file),
     host = on_host,
     port = on_port
   )
@@ -24,6 +75,21 @@ api_start <- function(on_host, on_port) {
 #' @return None, opens a browser to the requested URL
 #' @export
 api_open_doc <- function(url = plumber_url) {
+  # Argument validation relies on verify_args
+  if (exists("verify_args")) {
+    arg_check <- verify_args(
+      args       = as.list(environment()),
+      conditions = list(
+        url      = list(c("mode", "character"), c("length", 1))
+      ),
+      from_fn = "api_open_doc"
+    )
+    stopifnot(arg_check$valid)
+  } else {
+    stopifnot(is.character(plumber_url))
+    stopifnot(length(plumber_url) == 1)
+  }
+  
   docs <- "__docs__"
   if (!stringi::stri_detect(str = url, regex = docs)) {
     url <- sprintf("%s/%s/", url, docs)
@@ -48,6 +114,28 @@ api_open_doc <- function(url = plumber_url) {
 #' @return None, stops the plumber server
 #' @export
 api_stop <- function(pr = plumber_service, flush = TRUE, db_conn = "con", remove_service_obj = TRUE) {
+  # Argument validation relies on verify_args
+  if (exists("verify_args")) {
+    arg_check <- verify_args(
+      args       = as.list(environment()),
+      conditions = list(
+        pr           = list(c("length", 1), c("class", "r_process", "process", "R6")),
+        on_port      = list(c("length", 1)),
+        plumber_file = list(c("mode", "character"), "file_exists", c("length", 1))
+      ),
+      from_fn = "api_start"
+    )
+    stopifnot(arg_check$valid)
+  } else {
+    stopifnot(is.character(on_host))
+    stopifnot(length(on_host) == 1)
+    stopifnot(is.character(on_port))
+    stopifnot(length(on_port) == 1)
+    stopifnot(is.character(plumber_file))
+    stopifnot(length(plumber_file) == 1)
+    stopifnot(file.exists(plumber_file))
+  }
+  
   pr$kill()
   if (remove_service_obj) {
     rm(list = deparse(substitute(pr)), envir = .GlobalEnv)
@@ -83,6 +171,7 @@ api_reload <- function(pr = NULL, background = TRUE, on_host = NULL, on_port = N
       value = callr::r_bg(
         function() {
           source(file.path("plumber", "env_plumb.R"))
+          source(file.path("plumber", "api_control.R"))
           api_start(
             on_host = on_host,
             on_port = on_port
@@ -125,25 +214,40 @@ api_reload <- function(pr = NULL, background = TRUE, on_host = NULL, on_port = N
 #' Sanity check for plumber service name
 #'
 #' Provides a sanity check on whether or not a name reference exists and return
-#' its name if so. If not, return the default name of defined from
-#' `default_name`. This largely is used to prevent naming conflicts as part of
-#' managing the plumber service (thus the default name) but can be used for any
-#' item in the current namespace.
+#' its name if so. If not, return the default name defined from `default_name`.
+#' This largely is used to prevent naming conflicts as part of managing the
+#' plumber service but can be used for any item in the current namespace.
 #'
 #' @param obj R object or CHR scalar in question to be resolved in the namespace
-#' @param default_name CHR scalar name to use for `obj` if it does not exist.
+#' @param default_name CHR scalar name to use for `obj` if it does not exist
+#'   (default: NULL).
 #'
 #' @return CHR scalar of the resolved name
-obj_name_check <- function(obj, default_name = "plumber_service") {
-  if (is.character(obj)) {
-    pr_name <- obj
+#'
+#' @export
+#' @example
+#' \dontrun {\preformatted {
+#'   if (exists("log_it")) {
+#'     obj_name_check("test", "test")
+#'     test <- letters
+#'     obj_name_check(test)
+#'   }
+#' }}
+obj_name_check <- function(obj, default_name = NULL) {
+  obj_name <- deparse(substitute(obj)) %>%
+    str_remove_all('"')
+  if (exists(obj_name)) {
+    # Placeholder to do maybe do something like preservation/backup of current
+    # object or suggest a new name
+    
+    # pr <- eval(sym(obj_name))
   } else {
-    pr_name <- deparse(substitute(obj))
+    log_it("warn", glue('No object named "{obj_name}" exists. Defaulting to "{default_name}"'))
+    if (is.null(default_name)) {
+      log_it("warn", "No default name provided. Name given back as-is.")
+    } else {
+      obj_name <- default_name
+    }
   }
-  if (exists(pr_name)) {
-    pr <- eval(sym(pr_name))
-  } else {
-    pr_name <- default_name
-  }
-  return(pr_name)
+  return(obj_name)
 }
