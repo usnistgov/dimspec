c0f27df9b8301cc29ee92e37abe944a155a21033
Jared R <jared.ragland@nist.gov>
2022-06-21 11:35:07 -0400

    TODO: figure out why plumber isn't spinning up properly if launching the app to a virgin environment.

--- apps/spectral_match/global.R
+++ apps/spectral_match/global.R
@@ -2,6 +2,7 @@ dev <- TRUE
 vowels <- c("a", "e", "i", "o", "u")
 vowels <- c(vowels, toupper(vowels))
 APP_TITLE <- "NIST PFAS Database Spectra Match"
+app_dir <- "spectral_match"
 if (!exists("RENV_ESTABLISHED_SHINY") || !RENV_ESTABLISHED_SHINY) source(here::here("apps", "env_shiny.R"))
 source("app_functions.R")
 if (exists("LOGGING") && LOGGING_ON) {
@@ -25,15 +26,32 @@ if (exists("LOGGING") && LOGGING_ON) {
 }
 
 # Session variables
-if (exists("PLUMBER_URL") && api_endpoint(PLUMBER_URL, path = "active")) {
+if (exists("PLUMBER_URL")) {
+  try <- 1
+  alive <- try(api_endpoint(PLUMBER_URL, path = "active"))
+  if (inherits(alive, "try-error")) {
+    # This may execute faster than the plumber service is able to spin up, so pause for a bit and try it again.
+    # TODO The /correct/ way to treat this would be to poll the service object (no reference here) and wait until it reports that it is alive.
+    stop("API service does not appear to be available. Please run `api_reload()` and try again.")
+    # Sys.sleep(1)
+    # try <- try + 1
+    # alive <- try(api_endpoint(PLUMBER_URL, path = "active"))
+    # if (try == 5) {
+    #   warning("API endpoint was not active after 5 tries. Restarting API service.")
+    #   api_reload()
+    # }
+    # if (try == 10) {
+    #   stop("API endpoint not active after 10 tries. Aborting app spin up.")
+    # }
+  }
 } else {
   stop("This app requires an active API connection.")
 }
 
 app_settings <- list(
   experiment_types = api_endpoint(path = "table_search",
-                                   query = list(table_name = "norm_ms_n_types"),
-                                   return_format = "list") %>%
+                                  query = list(table_name = "norm_ms_n_types"),
+                                  return_format = "list") %>%
     lapply(function(x) {
       if (x$name == "none") {
         NULL 
@@ -44,8 +62,7 @@ app_settings <- list(
     purrr::flatten()
 )
 
-
-lapply(list.files("modals", pattern = ".R", full.names = TRUE),
+lapply(list.files(here::here("apps", app_dir, "modals"), pattern = ".R", full.names = TRUE),
        source)
 
 jscode <- HTML("
