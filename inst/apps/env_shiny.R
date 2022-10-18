704ee242e91e550173c9804c91f4d745b07c878f
Jared R <jared.ragland@nist.gov>
2022-10-18 10:12:23 -0400

    Remove rcdk as a dependency from shiny app env, not sure how this got in there.

--- inst/apps/env_shiny.R
+++ inst/apps/env_shiny.R
@@ -32,8 +32,7 @@ packs <- c("dplyr",
            "plotly",
            "httr",
            "readr",
-           "readxl",
-           "rcdk")
+           "readxl")
 packs_TRUE  <- which(packs %in% installed.packages())
 packs_FALSE <- packs[-packs_TRUE]
 if (length(packs_FALSE) > 0) {
